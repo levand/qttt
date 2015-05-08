39cf22c38aed4d5f228c6907542ca4cdc9340904
Luke VanderHart <luke@vanderhart.net>
2015-05-08 14:41:16 +0000

    Deleted Om and Quiescent code.

--- src/qttt/ui.cljs
+++ src/qttt/ui.cljs
@@ -1,18 +1,9 @@
 (ns qttt.ui
   (:require [goog.Uri :as uri]
-            [qttt.ui.om :as om]
-            [qttt.ui.reagent :as reagent]
-            [qttt.ui.quiescent :as quiescent]))
+            [qttt.ui.reagent :as reagent]))
 
 (enable-console-print!)
 
 (defn ^:export main
   []
-  (let [lib (keyword (.get (.getQueryData (goog.Uri. js/location)) "lib"))]
-    (case lib
-      :om (om/main)
-      :reagent (reagent/main)
-      :quiescent (quiescent/main)
-      (js/alert "Please use the query string to set 'lib' to om, reagent or quiescent. For example, 'dev.html?lib=om'"))))
-
-
+  (reagent/main))
