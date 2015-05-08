0d2148404bb8861b5fc51b0dc12bed50199d3337
Luke VanderHart <luke@vanderhart.net>
2015-05-08 15:31:00 +0000

    WIP adding game management to server

--- src/qttt/handler.clj
+++ src/qttt/handler.clj
@@ -9,8 +9,24 @@
      (update-in req [:uri]
                 #(if (= "/" %) "/dev.html" %)))))
 
+
 (defroutes site-routes
   (route/resources "/")
+  (POST "/game" []
+        ;; take an empty POST
+        ;; return a new game, including game ID
+        )
+  (POST "/move" []
+        
+        ;; take new move as EDN in POST
+        
+        ;; apply move to current game state
+        
+        ;; return new game state
+        
+       )
+  (GET "/state" []
+       )
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app (wrap-dir-index #'site-routes))
