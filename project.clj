e04a33c2d68c3166ee1534f3c86b72fd36190687
Luke VanderHart <luke@vanderhart.net>
2015-05-08 14:17:25 +0000

    got hello compojure working.

--- project.clj
+++ project.clj
@@ -6,8 +6,11 @@
                  [org.omcljs/om "0.8.8":exclusions [cljsjs/react]]
                  [cljsjs/react-with-addons "0.12.2-7"]
                  [quiescent "0.2.0-alpha1"]
-                 [com.cognitect/contextual "0.1.0"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
+                 [com.cognitect/contextual "0.1.0"]
+                 [compojure "1.3.3"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-ring "0.9.3"]]
+  :ring {:handler qttt.handler/app}
   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/gen"]
   :cljsbuild {:builds {:dev {:source-paths ["src"]
                              :compiler {:output-to "resources/public/gen/dev/main.js"
